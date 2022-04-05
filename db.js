3ef57c44fb4a031de8b2437761ad46fbf872f753
Jennifer Tang <jt3744@nyu.edu>
2022-04-05 03:16:50 -0400

    idk

--- db.js
+++ db.js
@@ -31,4 +31,4 @@ Category.plugin(URLSlugs('name'));
 mongoose.model('User', User);
 mongoose.model('Category', Category);
 mongoose.model('Item', Item);
-mongoose.connect('mongodb://localhost/expensedb');
+mongoose.connect('mongodb://localhost/grocerydb');
